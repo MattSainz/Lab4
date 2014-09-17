d29fb7394bee45203ee0325c78c3d1dd6eb27b9c
MattSainz <sainz.matt@gmail.com>
2014-09-17 13:37:26 -0600

    Update Bar.java

--- src/Foo/Bar.java
+++ src/Foo/Bar.java
@@ -1,9 +1,12 @@
 package Foo;
 
-public class Bar {
+public class Bar 
+{
 
-	public static void main(String[] args) {
-		System.out.println("I like NetBeans");
-	}
+  public static void main(String[] args) 
+  {
+	  System.out.println("I like NetBeans");
+	  System.out.println("Hello From GitHub");
+  }
 
 }
